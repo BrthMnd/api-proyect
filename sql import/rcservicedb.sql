477356b654f817c75a424d699565c90215257a46
Brandon Quintero <developerbvth@gmail.com>
2023-04-30 23:49:50 -0500

    Base de datos Actualizada

--- sql import/rcservicedb.sql
+++ sql import/rcservicedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-04-2023 a las 23:16:07
+-- Tiempo de generación: 01-05-2023 a las 06:48:02
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `rcservicedb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `propietario`
+--
+
+CREATE TABLE `propietario` (
+  `Id_propietario` varchar(20) NOT NULL,
+  `Numero_Identificacion_D` varchar(15) NOT NULL,
+  `Nombres` varchar(20) NOT NULL,
+  `Apellidos` varchar(20) NOT NULL,
+  `TelefonoUCelular` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `propietario`
+--
+
+INSERT INTO `propietario` (`Id_propietario`, `Numero_Identificacion_D`, `Nombres`, `Apellidos`, `TelefonoUCelular`) VALUES
+('1001', '1038', 'Dufainer  Andres', 'Esquivel Bertel', '3123555'),
+('2222', '2323', 'Dufainer  Andres', 'Esquivel Bertel', '3123555');
 
 -- --------------------------------------------------------
 
@@ -44,6 +66,12 @@ INSERT INTO `tabla_prueba` (`id`, `nombre`, `edad`) VALUES
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `propietario`
+--
+ALTER TABLE `propietario`
+  ADD PRIMARY KEY (`Id_propietario`);
 
 --
 -- Indices de la tabla `tabla_prueba`
