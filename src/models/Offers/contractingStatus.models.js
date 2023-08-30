ad1f74fbba1a4e7d65dfb4a6991f956e5a4695f4
Brandon Quintero <developerbvth@gmail.com>
2023-08-30 02:18:25 -0500

    datos offers actualizados

--- src/models/Offers/contractingStatus.models.js
+++ src/models/Offers/contractingStatus.models.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ContractingStatusSchema = new Schema({
-    name: {type: String, },
-    description:{type: String, }, 
+  name: { type: String },
+  description: { type: String },
 });
 
-const ContractingStatusModel = mongoose.model('ContratingStatus', ContractingStatusSchema );
+const ContractingStatusModel = mongoose.model(
+  "ContratingStatus",
+  ContractingStatusSchema
+);
 
-module.exports = { ContractingStatusModel }
+module.exports = { ContractingStatusModel };
