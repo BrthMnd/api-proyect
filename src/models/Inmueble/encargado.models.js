c088d82ef0cb2814a548d4c7331c852e5c16bbdf
Brandon Quintero <developerbvth@gmail.com>
2023-09-06 01:28:48 -0500

    Duf Terminado, att:b.

--- src/models/Inmueble/encargado.models.js
+++ src/models/Inmueble/encargado.models.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const EncargadoSchema = new Schema(
-    {
-        documento: {type: Number, required: true},
-        nombres: { type: String, required: true},
-        apellidos: { type: String, required: true},
-        correo: {type: String, required: true},
-        telefono: { type: String, required: true},
-        estado: { type: Boolean, default: true},
-        direccion: { type: String, required: true},
-        fechCreacion: { type: Date, default: Date.now() },
-    },
-    {
-        versionKey: false, // __v: 0
-    }
+  {
+    documento: { type: Number },
+    nombres: { type: String },
+    apellidos: { type: String },
+    correo: { type: String },
+    telefono: { type: String },
+    estado: { type: Boolean, default: true },
+    direccion: { type: String },
+    fechCreacion: { type: Date, default: Date.now() },
+  },
+  {
+    versionKey: false, // __v: 0
+  }
 );
 
-const EncargadoModels = mongoose.model('Encargados', EncargadoSchema, 'encargados')
+const EncargadoModels = mongoose.model(
+  "Encargados",
+  EncargadoSchema,
+  "encargados"
+);
 
 module.exports = {
-    EncargadoModels,
-}
+  EncargadoModels,
+};
--- src/models/Inmueble/inmueble.models.js
+++ src/models/Inmueble/inmueble.models.js
@@ -5,26 +5,32 @@ const { EncargadoModels } = require("./encargado.models");
 
 const InmuebleSchema = new Schema(
   {
-    tipoPropiedad: { type: String},
-    direccion: { type: String},
-    metrosCuadrados: { type: Number},
+    tipoPropiedad: { type: String },
+    direccion: { type: String },
+    metrosCuadrados: { type: Number },
     nHabitaciones: { type: Number },
-    nBanos: { type: Number},
-    fechConstruccion: { type: Date},
+    nBanos: { type: Number },
+    fechConstruccion: { type: Date },
     plano: {
       type: String,
       deafault:
         "https://misplanosdecasas.com/wp-content/uploads/2020/11/30-Planos-de-Casas-para-construir.pdf",
     },
-    id_propietario: { type: Schema.Types.ObjectId, ref: PropietarioModels },
-    id_encargado: { type: Schema.Types.ObjectId, ref: EncargadoModels },
+    id_propietario: {
+      type: Schema.Types.ObjectId,
+      ref: PropietarioModels.modelName,
+    },
+    id_encargado: {
+      type: Schema.Types.ObjectId,
+      ref: EncargadoModels.modelName,
+    },
     fechCreacion: { type: Date, default: Date.now() },
   },
   {
     versionKey: false,
   }
 );
-const InmuebleModels = mongoose.model("Inmuebles", InmuebleSchema, "inmuebles"); // 
+const InmuebleModels = mongoose.model("Inmuebles", InmuebleSchema, "inmuebles"); //
 
 module.exports = {
   InmuebleModels,
