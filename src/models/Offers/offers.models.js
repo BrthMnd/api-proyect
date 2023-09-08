87570a433c4ae622b09dfac9ca8c0a2df1fa14c6
Brandon Quintero <developerbvth@gmail.com>
2023-09-08 17:40:21 -0500

    actualizacion de servicios en ofertasmodels

--- src/models/Offers/offers.models.js
+++ src/models/Offers/offers.models.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
+const { FechaActual } = require("../../tools/date.tools");
 const Schema = mongoose.Schema;
 const { InmuebleModels } = require("../Inmueble/inmueble.models");
 const { StatusModel } = require("./status.models");
+const { ServicioModels } = require("../Proveedores/servicios.models");
 
 let OffersSchema = new Schema({
-  publicationDate: { type: Date, default: Date.now() },
+  publicationDate: { type: String, default: FechaActual },
   description: { type: String, maxLength: 1000 },
   id_property: { type: Schema.Types.ObjectId, ref: InmuebleModels.modelName },
   id_status: { type: Schema.Types.ObjectId, ref: StatusModel.modelName },
+  id_service: { type: Schema.Types.ObjectId, ref: ServicioModels.modelName },
 });
 
 let OffersModel = mongoose.model("Offers", OffersSchema);
