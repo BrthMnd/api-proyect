8a54e27b9280f375871cc2ec6c8a5d2575074852
Cristian Pardo <cristianpardo044@gmail.com>
2023-09-30 00:59:17 -0500

    Haciendo Validaciones de eliminacion en servicio y categoria

--- src/controllers/proveedores/servicios.controllers.js
+++ src/controllers/proveedores/servicios.controllers.js
@@ -1,6 +1,8 @@
 const { ObjectId } = require("mongodb");
 const { ServicioModels } = require("../../models/Proveedores/servicios.models");
 
+const { OffersModel } = require("../../models/Offers/offers.models");
+
 class ServiciosController {
   async getServicios(req, res, next) {
     try {
@@ -50,37 +52,6 @@ class ServiciosController {
 
   //______________________________________________________________________________________
 
-  // async putServicio(req, res, next) {
-  //   const { Nombre_Servicio, Descripcion, estado, Categoria_Servicios } =
-  //     req.body;
-  //   const id = req.params.id;
-  //   try {
-  //     const result = await ServicioModels.findOneAndUpdate(
-  //       { _id: new ObjectId(id) },
-  //       {
-  //         $set: {
-  //           Nombre_Servicio: Nombre_Servicio,
-  //           Descripcion: Descripcion,
-  //           estado: estado,
-  //           Categoria_Servicios: Categoria_Servicios,
-  //         },
-  //       }
-  //     );
-
-  //     if (result) {
-  //       res
-  //         .status(200)
-  //         .json({ message: "Documento actualizado exitosamente\n", result });
-  //     } else {
-  //       res.status(500).json({ error: "Error al actualizar el documento" });
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   } finally {
-  //     next();
-  //   }
-  // }
-
   async putServicio(req, res, next) {
     const Update = req.body;
     const id = req.params.id;
@@ -105,26 +76,58 @@ class ServiciosController {
     }
   }
 
-
-
   //______________________________________________________________________________________
 
   async deleteServicio(req, res, next) {
     const id = req.params.id;
-    try {
-      const result = await ServicioModels.deleteOne({ _id: new ObjectId(id) });
 
-      if (result) {
-        res.status(200).send({ message: "Borrado con éxito" });
+    try {
+      const reference = await OffersModel.find({
+        id_service: new ObjectId(id),
+      });
+
+      console.log(reference);
+
+      if (reference.length > 0) {
+        res.status(500).send({
+          error:
+            "No se puede eliminar esta categoría, ya que se utiliza en otra parte.",
+        });
       } else {
-        res.status(500).send({ error: "Error al eliminar el archivo" });
+        const result = await ServicioModels.findOneAndDelete({
+          _id: new ObjectId(id),
+        });
+
+        if (result) {
+          res.status(200).send({ message: "Categoría borrada con éxito" });
+        } else {
+          res.status(500).send({ error: "Error al eliminar la categoría" });
+        }
       }
     } catch (error) {
-      console.log(error);
+      console.log("Error al eliminar la categoría -> " + error.message);
+      res.status(500).send({ error: "Error al eliminar la categoría" });
     } finally {
       next();
     }
   }
+
+  // async deleteServicio(req, res, next) {
+  //   const id = req.params.id;
+  //   try {
+  //     const result = await ServicioModels.deleteOne({ _id: new ObjectId(id) });
+
+  //     if (result) {
+  //       res.status(200).send({ message: "Borrado con éxito" });
+  //     } else {
+  //       res.status(500).send({ error: "Error al eliminar el archivo" });
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     next();
+  //   }
+  // }
 }
 
 module.exports = { ServiciosController };
