9247d913493570a5e16d94798d2a244849a78f98
Brandon Quintero <developerbvth@gmail.com>
2023-10-26 05:39:54 -0500

    prueba de cors

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 // * Require
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const route = require("./src/routes/"); // <- Rutas
 const { connect, disconnect, notFound } = require("./src/middlewares");
 const { CORS } = require("./src/config/cors.config");
 const logger = require("morgan");
 // * Configuration
 app.use(bodyParser.json());
-app.use(CORS);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(logger("dev"));
-app.use(cookieParser())
+app.use(cookieParser());
 
 //middleware
 app.use(connect);
