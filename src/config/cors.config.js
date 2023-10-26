7ddef7846c1d4132f33fd066abd208ff472ec35b
Brandon Quintero <developerbvth@gmail.com>
2023-10-26 04:01:03 -0500

    ALLOW CREDENCIALS

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,6 +1,7 @@
 function CORS(req, res, next) {
   res.append("Access-Control-Allow-Origin", "http://localhost:5173");
   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Credentials", "true");
   res.append(
     "Access-Control-Allow-Headers",
     "Origin, Content-Type, X-Auth-Token, Authorization"
