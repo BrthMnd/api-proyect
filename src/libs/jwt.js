629784d70c7441c0351d9f4e99ebc93d18370133
Brandon Quintero <developerbvth@gmail.com>
2023-11-18 16:03:51 -0500

    b *

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -2,18 +2,13 @@ const jwt = require("jsonwebtoken");
 const Secret = process.env.SECRET_KEY || "hohohoho";
 
 async function CreateAccess(payload) {
-    try {
-        const token = await jwt.sign(
-            payload,
-            Secret,
-            {
-              expiresIn: "1d",
-            }
-          )
-          return token
-    } catch (error) {
-        return error
-    }
-  
+  try {
+    const token = await jwt.sign(payload, Secret, {
+      expiresIn: "1d",
+    });
+    return token;
+  } catch (error) {
+    return error;
+  }
 }
-module.exports = {CreateAccess}
+module.exports = { CreateAccess };
