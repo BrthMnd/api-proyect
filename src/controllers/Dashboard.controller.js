c57d4d04c139361657331ae54218eeeeec4aefe0
jeni bejarano <jenim@JENI0708.(none)>
2023-11-22 12:48:26 -0500

    Dashboard 10%

--- src/controllers/Dashboard.controller.js
+++ src/controllers/Dashboard.controller.js
@@ -1,21 +1,55 @@
+// const { CategoriaModel } = require("../models/Proveedores/categoria.models")
+// const { ProveedoresModels } = require("../models/Proveedores/provedores.models")
+// const { ServicioModels } = require("../models/Proveedores/servicios.models")
+
+// class Dashboard {
+//     async Grafics_Get (req,res,next){
+//         try {
+//             const categoria = await CategoriaModel.find()
+//             const servicios = await ServicioModels.find()
+//             const proveedor = await ProveedoresModels.find()
+               
+
+
+//             res.status(200).send('hola')
+//         } catch (error) {
+//             console.log(error)
+//             return res.status(500).send('Error al obtener los datos')
+//         }
+//     }
+// }
+// module.exports = {Dashboard}
+
+
+const { InmuebleModels } = require("../models/Inmueble/inmueble.models")
 const { CategoriaModel } = require("../models/Proveedores/categoria.models")
 const { ProveedoresModels } = require("../models/Proveedores/provedores.models")
 const { ServicioModels } = require("../models/Proveedores/servicios.models")
 
 class Dashboard {
-    async Grafics_Get (req,res,next){
+    async Grafics_Get(req, res, next) {
         try {
-            const categoria = await CategoriaModel.find()
+            const categorias = await CategoriaModel.find()
             const servicios = await ServicioModels.find()
-            const proveedor = await ProveedoresModels.find()
-               
+            const proveedores = await ProveedoresModels.find()
+            const inmuebles = await InmuebleModels.find()
+            
 
+            // Crear un objeto con los datos obtenidos
+            const data = {
+                categorias: categorias,
+                servicios: servicios,
+                proveedores: proveedores,
+                inmuebles:inmuebles
+            };
 
-            res.status(200).send('hola')
+            // Enviar el objeto como respuesta en formato JSON
+            res.status(200).json(data);
         } catch (error) {
-            console.log(error)
-            return res.status(500).send('Error al obtener los datos')
+            console.log(error);
+            return res.status(500).send('Error al obtener los datos');
         }
     }
 }
-module.exports = {Dashboard}
+
+module.exports = { Dashboard }
