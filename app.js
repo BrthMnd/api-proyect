2a2eabfcc093321d4fe8cf618d4597a9c423eabf
Cristian Pardo <cristianpardo044@gmail.com>
2023-12-05 03:03:37 -0500

    *

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const route = require("./src/routes/"); // <- Rutas
-const {  notFound } = require("./src/middlewares");
+const { notFound } = require("./src/middlewares");
 const { CORSConfiguration } = require("./src/config/cors.config");
 const logger = require("morgan");
 const db = require("./src/config/db");
@@ -15,9 +15,12 @@ app.use(bodyParser.json());
 app.use(cors(CORSConfiguration));
 app.use(logger("dev"));
 app.use(cookieParser());
-db.on('error', console.error.bind(console, 'Error de conexión a la base de datos:'));
-db.once('open', () => {
-  console.log('Conexión exitosa a la base de datos');
+db.on(
+  "error",
+  console.error.bind(console, "Error de conexión a la base de datos:")
+);
+db.once("open", () => {
+  console.log("Conexión exitosa a la base de datos");
 });
 
 //middleware
