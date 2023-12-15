2ff7e2e296d286b086d1686b17bd3e6aba66369b
Brandon Quintero <developerbvth@gmail.com>
2023-12-15 14:43:39 -0500

    ---

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://hoppscotch.io",
   "http://localhost:19006",
-  "https://657ca922c7a0a526e6939e11--aesthetic-manatee-1a0ece.netlify.app/",
+  "https://657ca922c7a0a526e6939e11--aesthetic-manatee-1a0ece.netlify.app",
 ];
 const CORSConfiguration = {
   origin: allowedOrigins,
